cbff6a18dd6121dd1340f84c0cabd608258fea3b
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2019-04-08 00:21:58 +0200

    repo init

---
+++ config.h
@@ -0,0 +1,36 @@
+/* if you don't want to screw your colors stay away from this */
+#define CLR_R(x)   (((x) & 0xff0000) >> 16)
+#define CLR_G(x)   (((x) & 0x00ff00) >>  8)
+#define CLR_B(x)   (((x) & 0x0000ff) >>  0)
+#define CLR_16(x)  ((double)(x) / 0xff)
+#define CLR_GDK(x) (const GdkRGBA){ .red = CLR_16(CLR_R(x)), \
+                                    .green = CLR_16(CLR_G(x)), \
+                                    .blue = CLR_16(CLR_B(x)), \
+                                    .alpha = 0 }
+
+/* this is safe to mess with */
+#define TITLE "qterm"
+
+#define FONT        "Terminus"
+#define FONT_SCALE  0.75
+#define CURSOR_AH   TRUE
+#define REWRAP      TRUE
+#define BELL        FALSE
+
+#define BACKGROUND  0x111111
+#define CLR_0       0x1C1C1C
+#define CLR_1       0xD81860
+#define CLR_2       0x60FF60
+#define CLR_3       0xF9FD75
+#define CLR_4       0x4695C8
+#define CLR_5       0xA78EDB
+#define CLR_6       0x43AFCE
+#define CLR_7       0xF3EBE2
+#define CLR_8       0x1C1C1C
+#define CLR_9       0xD81860
+#define CLR_10      0x60FF60
+#define CLR_11      0xF9FD75
+#define CLR_12      0x4695C8
+#define CLR_13      0xA78EDB
+#define CLR_14      0x43AFCE
+#define CLR_15      0xF3EBE2
---
+++ term.c
@@ -0,0 +1,70 @@
+#include <vte/vte.h>
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window, *terminal;
+    PangoFontDescription *df;
+
+    /* Initialise GTK, window, terminal and font */
+    gtk_init(&argc, &argv);
+    terminal = vte_terminal_new();
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), TITLE);
+    df = pango_font_description_new();
+    
+    /* font settings */
+    pango_font_description_set_size(df, 10 * PANGO_SCALE);
+    pango_font_description_set_weight(df, PANGO_WEIGHT_BOLD);
+    pango_font_description_set_family(df, FONT);
+
+    /* terminal settings */
+    vte_terminal_set_rewrap_on_resize(VTE_TERMINAL(terminal), REWRAP);
+    vte_terminal_set_mouse_autohide(VTE_TERMINAL(terminal), CURSOR_AH);
+    vte_terminal_set_font_scale(VTE_TERMINAL(terminal), FONT_SCALE);
+    vte_terminal_set_font(VTE_TERMINAL(terminal), df);
+    vte_terminal_set_audible_bell(VTE_TERMINAL(terminal), BELL);
+
+    /* 16 color support */
+    vte_terminal_set_colors(VTE_TERMINAL(terminal),
+    &CLR_GDK(CLR_7),
+    &(GdkRGBA){ .alpha = 1 },
+    (const GdkRGBA[]){
+        CLR_GDK(CLR_0),
+        CLR_GDK(CLR_1),
+        CLR_GDK(CLR_2),
+        CLR_GDK(CLR_3),
+        CLR_GDK(CLR_4),
+        CLR_GDK(CLR_5),
+        CLR_GDK(CLR_6),
+        CLR_GDK(CLR_7),
+        CLR_GDK(CLR_8),
+        CLR_GDK(CLR_9),
+        CLR_GDK(CLR_10),
+        CLR_GDK(CLR_11),
+        CLR_GDK(CLR_12),
+        CLR_GDK(CLR_13),
+        CLR_GDK(CLR_14),
+        CLR_GDK(CLR_15)}, 16);
+    
+    /* new shell */
+    gchar **envp = g_get_environ();
+    gchar **command = (gchar *[]){g_strdup(g_environ_getenv(envp, "SHELL")), NULL };
+    g_strfreev(envp);
+    vte_terminal_spawn_sync(VTE_TERMINAL(terminal),
+        VTE_PTY_DEFAULT,
+        NULL,
+        command,
+        NULL,
+        0,
+        NULL, NULL,
+        NULL,
+        NULL, NULL);
+
+    /* exit on close */
+    g_signal_connect(terminal, "child-exited", gtk_main_quit, NULL);
+
+    /* put it togheter */
+    gtk_container_add(GTK_CONTAINER(window), terminal);
+    gtk_widget_show_all(window);
+    gtk_main();
+}
